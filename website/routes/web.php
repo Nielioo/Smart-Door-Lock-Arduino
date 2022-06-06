df0986a9d1ad3345e71734bb3bed84b95b400625
Bryan <bryanmuliawan@yahoo.co.id>
2022-06-06 15:36:37 +0700

    Change home controller

--- website/routes/web.php
+++ website/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +17,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
 Auth::routes();
+Route::middleware(['auth'])->group(function() {
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+	// Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+	Route::resources([
+		'/home' => HomeController::class,
+		'/main' => MainController::class,
+	]);
+});
