8a1d5ac8615c6bf3af6dd6651123cb0bdae84678
Daniel Aprillio <danielaprillio02@gmail.com>
2022-06-06 16:36:27 +0700

    Update dashboard.blade.php

--- website/resources/views/dashboard.blade.php
+++ website/resources/views/dashboard.blade.php
@@ -8,8 +8,10 @@
     <title>Dashboard</title>
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('../css/dashboard.css') }}">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
     <link rel="icon" type="image/png" href="{{asset('../image/icon.png')}}"/>
     <script src="https://kit.fontawesome.com/03f2b35007.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
 </head>
 
 <body id="page-top">
@@ -72,7 +74,7 @@
                 <!-- End of Topbar -->
 
                 <!-- Table -->
-                <!-- DataTales Example -->
+                <!-- DataTables Example -->
                 <div class="card shadow mb-4">
                     <div class="card-header py-3">
                         <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
